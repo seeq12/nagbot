61bfd4d3c653b43c975381f5bccfe257e3d434fd
Quinn Russell <quinn.russell@seeq.com>
2022-06-13 17:14:04 -0500

    CRAB-30171: added extra test cases

--- tests/test_sqaws.py
+++ tests/test_sqaws.py
@@ -95,47 +95,47 @@ def test_terminate_instance_exception(mock_client):
     mock_ec2.terminate_instances.assert_called_once_with(InstanceIds=[instance_id])
 
 
-@pytest.mark.parametrize('stop_terminate_dict, expected_stop_result, expected_terminate_result, '
-                         'expected_stop_tag_name, expected_terminate_tag_name', [
-                             ({'stop after': '2022-05-10', 'terminate after': '2022-05-11'}, '2022-05-10', '2022-05-11',
-                              'stop after', 'terminate after'),
-                             ({'Stop After': '2030-07-23', 'Terminate After': '2050-08-10'}, '2030-07-23', '2050-08-10',
-                              'Stop After', 'Terminate After'),
-                             ({'STOP AFTER': '2021-03-04', 'TERMINATE AFTER': '2022-09-12'}, '2021-03-04', '2022-09-12',
-                              'STOP AFTER', 'TERMINATE AFTER'),
-                             ({'stop_after': '2022-05-10', 'terminate_after': '2022-05-11'}, '2022-05-10', '2022-05-11',
-                              'stop_after', 'terminate_after'),
-                             ({'Stop_After': '2030-07-23', 'Terminate_After': '2050-08-10'}, '2030-07-23', '2050-08-10',
-                              'Stop_After', 'Terminate_After'),
-                             ({'STOP_AFTER': '2021-03-04', 'TERMINATE_AFTER': '2022-09-12'}, '2021-03-04', '2022-09-12',
-                              'STOP_AFTER', 'TERMINATE_AFTER'),
-                             ({'stopafter': '2022-05-10', 'terminateafter': '2022-05-11'}, '2022-05-10', '2022-05-11',
-                              'stopafter', 'terminateafter'),
-                             ({'StopAfter': '2030-07-23', 'TerminateAfter': '2050-08-10'}, '2030-07-23', '2050-08-10',
-                              'StopAfter', 'TerminateAfter'),
-                             ({'STOPAFTER': '2021-03-04', 'TERMINATEAFTER': '2022-09-12'}, '2021-03-04', '2022-09-12',
-                              'STOPAFTER', 'TERMINATEAFTER'),
-                             ({'': '2021-03-04', 'terminate.after': '2022-09-12'}, '', '2022-09-12', '',
-                              'terminate.after'),
-                             ({'stop.after': '2021-03-04', '': '2022-09-12'}, '2021-03-04', '', 'stop.after', '')
+@pytest.mark.parametrize('test_dict, expected_stop_result, expected_terminate_result, expected_nagbot_state, '
+                         'expected_stop_tag_name, expected_terminate_tag_name, expected_nagbot_state_tag_name', [
+                             ({'stop after': '2022-05-10', 'terminate after': '2022-05-11', 'nagbot state': 'running'},
+                              '2022-05-10', '2022-05-11', 'running', 'stop after', 'terminate after', 'nagbot state'),
+                             ({'Stop After': '2030-07-23', 'Terminate After': '2050-08-10', 'Nagbot State': 'running'},
+                              '2030-07-23', '2050-08-10', 'running', 'Stop After', 'Terminate After', 'Nagbot State'),
+                             ({'STOP AFTER': '2021-03-04', 'TERMINATE AFTER': '2022-09-12', 'NAGBOT STATE': 'running'},
+                              '2021-03-04', '2022-09-12', 'running', 'STOP AFTER', 'TERMINATE AFTER', 'NAGBOT STATE'),
+                             ({'stop_after': '2022-05-10', 'terminate_after': '2022-05-11', 'nagbot_state': 'running'},
+                              '2022-05-10', '2022-05-11', 'running', 'stop_after', 'terminate_after', 'nagbot_state'),
+                             ({'Stop_After': '2030-07-23', 'Terminate_After': '2050-08-10', 'Nagbot_State': 'running'},
+                              '2030-07-23', '2050-08-10', 'running', 'Stop_After', 'Terminate_After', 'Nagbot_State'),
+                             ({'STOP_AFTER': '2021-03-04', 'TERMINATE_AFTER': '2022-09-12', 'NAGBOT_STATE': 'running'},
+                              '2021-03-04', '2022-09-12', 'running', 'STOP_AFTER', 'TERMINATE_AFTER', 'NAGBOT_STATE'),
+                             ({'stopafter': '2022-05-10', 'terminateafter': '2022-05-11', 'nagbotstate': 'running'},
+                              '2022-05-10', '2022-05-11', 'running', 'stopafter', 'terminateafter', 'nagbotstate'),
+                             ({'StopAfter': '2030-07-23', 'TerminateAfter': '2050-08-10', 'NagbotState': 'running'},
+                              '2030-07-23', '2050-08-10', 'running', 'StopAfter', 'TerminateAfter', 'NagbotState'),
+                             ({'STOPAFTER': '2021-03-04', 'TERMINATEAFTER': '2022-09-12', 'NAGBOTSTATE': 'running'},
+                              '2021-03-04', '2022-09-12', 'running', 'STOPAFTER', 'TERMINATEAFTER', 'NAGBOTSTATE'),
+                             ({'': '2021-03-04', 'TerminateAfter': '2022-09-12', 'NagbotState': 'running'},
+                              '', '2022-09-12', 'running', 'StopAfter', 'TerminateAfter', 'NagbotState'),
+                             ({'StopAfter': '2021-03-04', '': '2022-09-12', 'NagbotState': 'running'},
+                              '2021-03-04', '', 'running', 'StopAfter', 'TerminateAfter', 'NagbotState'),
+                             ({'StopAfter': '2021-03-04', 'TerminateAfter': '2022-09-12', '': 'running'},
+                              '2021-03-04', '2022-09-12', '', 'StopAfter', 'TerminateAfter', 'NagbotState')
                          ])
-def test_stop_and_terminate_after(stop_terminate_dict, expected_stop_result,
-                                  expected_terminate_result, expected_stop_tag_name, expected_terminate_tag_name):
+def test_get_tag_names(test_dict, expected_stop_result, expected_terminate_result, expected_nagbot_state,
+                       expected_stop_tag_name, expected_terminate_tag_name, expected_nagbot_state_tag_name):
     stop_after_tag_name, terminate_after_tag_name, nagbot_state_tag_name = \
-        app.sqaws.get_tag_names(stop_terminate_dict)
-    stop_after = stop_terminate_dict.get(stop_after_tag_name, '')
-    terminate_after = stop_terminate_dict.get(terminate_after_tag_name, '')
+        app.sqaws.get_tag_names(test_dict)
+    stop_after = test_dict.get(stop_after_tag_name, '')
+    terminate_after = test_dict.get(terminate_after_tag_name, '')
+    nagbot_state = test_dict.get(nagbot_state_tag_name, '')
 
+    # Ensure tag value is correct
     assert stop_after == expected_stop_result
     assert terminate_after == expected_terminate_result
+    assert nagbot_state == expected_nagbot_state
 
     # Ensure tag name is set to default if empty string is passed in, otherwise should be the specified tag name
-    if expected_stop_tag_name == '':
-        assert stop_after_tag_name == 'StopAfter'
-    else:
-        assert stop_after_tag_name == expected_stop_tag_name
-    if expected_terminate_tag_name == '':
-        assert terminate_after_tag_name == 'TerminateAfter'
-    else:
-        assert terminate_after_tag_name == expected_terminate_tag_name
-    assert nagbot_state_tag_name == 'NagbotState'
+    assert stop_after_tag_name == expected_stop_tag_name
+    assert terminate_after_tag_name == expected_terminate_tag_name
+    assert nagbot_state_tag_name == expected_nagbot_state_tag_name
